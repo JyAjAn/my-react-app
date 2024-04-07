b5ca75f28076d5a322e6258380bdbd27752cb0c4
Charlie Le <161328512+JyAjAn@users.noreply.github.com>
2024-04-07 15:56:13 +0200

    props.2

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,29 @@ import './App.css'
 import PokemonCard from "./Components/PokemonCard"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+export default App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+      imgSrc:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png"
+    },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <di>
+<di>
   <PokemonCard/>
 </di>
-    </>
-  )
-}
 
-export default App
+  )
+    <>
+      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[1]} />
+    </>
+  );
+}
