3a2faf92eeefdf15016c3bc8ec83fc176eefe348
Charlie Le <161328512+JyAjAn@users.noreply.github.com>
2024-04-07 16:20:37 +0200

    state.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,39 @@
-import './App.css';
-import PokemonCard from "./Components/PokemonCard";
-import { useState } from "./Components/PokemonCard";
-import NavBar from "./Components/NavBar";
+import { useState } from "react";
+
+import NavBar from "./components/NavBar";
+import PokemonCard from "./components/PokemonCard";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
-  const [pokemonIndex, setpokemonIndex] = useState(0);
-  // const pokemonList = [
-  //   {
-  //     name: "Bulbasaur",
-// @@ -12,12 +12,39 @@ function App() {
-  //   },
-  //   {
-  //     name: "Mew",
-  //     imgSrc:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png"
-  //     imgSrc: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png",
-  //   },
-  //   {
-  //     name: "charmander",
-  //     imgSrc:
-  //       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-  //   },
-  //   {
-  //     name: "squirtle",
-  //     imgSrc:
-  //       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-  //   },
-  //   {
-  //     name: "pikachu",
-  //     imgSrc:
-  //       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-  //   },
-  // ];
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
   return (
-    <>
-      {/* <PokemonCard pokemon={pokemonList[1]} /> */}
+    <div>
+      <NavBar pokemonIndex={pokemonIndex} setPokemonIndex={setPokemonIndex} pokemonList={pokemonList} />
       <PokemonCard pokemon={pokemonList[pokemonIndex]} />
-      <NavBar pokemonIndex = {pokemonIndex} pokemonList= {pokemonList} setpokemonIndex={setpokemonIndex}/>
-
-      {pokemonIndex > 0 ? (
-        <button onClick={() => setpokemonIndex(pokemonIndex - 1)}>
-          Précedent
-        </button>
-      ) : null}
-
-      {pokemonIndex < pokemonList.length - 1 && (
-        <button onClick={() => setpokemonIndex(pokemonIndex + 1)}>
-          Suivant
-        </button>
-      )}
-    </>
+    </div>
   );
 }
+
+export default App;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,17 +1,35 @@
-function NavBar ({pokemonIndex, pokemonList, setpokemonIndex}) {
-    return (
-        <>
-                {pokemonIndex > 0 ? (
-        <button onClick={() => setpokemonIndex(pokemonIndex - 1)}>
-        Précedent
-        </button>
-    ) : null}
+import PropTypes from "prop-types";
 
-    {pokemonIndex < pokemonList.length - 1 && (
-        <button onClick={() => setpokemonIndex(pokemonIndex + 1)}>
-        Suivant
+function NavBar({ pokemonIndex, setPokemonIndex, pokemonList }) {
+  const pickPrevious = () => setPokemonIndex(pokemonIndex - 1);
+
+  const pickNext = () => setPokemonIndex(pokemonIndex + 1);
+
+  return (
+    <nav>
+      {pokemonIndex > 0 && (
+        <button type="button" onClick={pickPrevious}>
+          previous
         </button>
-    )}
-        </>
-    )
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button type="button" onClick={pickNext}>
+          next
+        </button>
+      )}
+    </nav>
+  );
 }
+
+NavBar.propTypes = {
+  pokemonIndex: PropTypes.number.isRequired,
+  setPokemonIndex: PropTypes.func.isRequired,
+  pokemonList: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string,
+    })
+  ).isRequired,
+};
+
+export default NavBar;
